69590300220b0c86138c9a1e43ee557d485c91be
AngelJTumG <javierangel43.logmail.com>
2026-09-25 11:01:50 -0600

    feat: "commit de configuracion de docker"

---
+++ src/main/java/com/gestionclubes/GestionClubesApplication.java
@@ -0,0 +1,12 @@
+package com.gestionclubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClubesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GestionClubesApplication.class, args);
+    }
+}
+
